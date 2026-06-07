9e2675db6fc66081ea1d627203fbb0e15d7f30da
Sebastián Rojas <sebastian.rojas2@virginiogomez.cl>
2026-06-06 22:39:46 -0400

    Resuelve conflicto en AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+<<<<<<< HEAD
 use App\Models\Panel;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+=======
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use app\Models\User;
+>>>>>>> 9f2303bfaa1d5c2fa410f919cf591f8f26546df6
 
 class AuthController extends Controller
 {
@@ -20,6 +26,7 @@ class AuthController extends Controller
         return view('auth.register');
     }
 
+<<<<<<< HEAD
     public function main_page()
     {
         return view('Main');
@@ -72,5 +79,30 @@ class AuthController extends Controller
     {
         $panel = Panel::whereUserId(Auth::id())->first();
         return view('panel_de_control', compact('panel'));
+=======
+    public function store(Request $request)
+    {
+        $request->validate([
+            // Añadimos 'unique:users' para que verifique si el nombre ya existe
+            'user_name' => 'required|unique:users,user_name',
+            'password' => 'required|min:6',
+        ], [
+            // Mensaje personalizado (opcional)
+            'user_name.unique' => 'Este nombre de usuario ya está en uso, elige otro.',
+        ]);
+
+        $user = User::create([
+            'user_name' => $request->user_name,
+            'password' => bcrypt($request->password),
+        ]);
+
+        Panel::create([
+            'user_id'    => $user->id,
+            'background' => 'images/background/Wall_Windows.jpg',
+            'avatar'     => 'images/user_icon/user_icon_1.png',
+        ]);
+
+        return redirect()->route('login')->with('was_created', true);
+>>>>>>> 9f2303bfaa1d5c2fa410f919cf591f8f26546df6
     }
 }
